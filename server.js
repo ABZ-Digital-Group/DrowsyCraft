58d2c16277fd11528dbc7417e727b139dd3fa4b6
stuart Keith <stuartiek@gmail.com>
2026-03-15 19:50:50 +0000

    start

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// --- CONFIGURATION ---
+const PORT = process.env.PORT || 3000;
+
+// --- VIEW ENGINE ---
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// --- STATIC ASSETS ---
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// --- ROUTES ---
+app.get('/', (req, res) => {
+    res.render('pages/index'); 
+});
+
+// --- START SERVER ---
+app.listen(PORT, () => {
+    console.log(`🚀 DrowsyCraft Web Panel running on port ${PORT}`);
+});
